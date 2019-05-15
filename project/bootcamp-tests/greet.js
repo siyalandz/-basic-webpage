83fc35dd64567986414387cd83ee3cdd6562766a
Siyanda <siyalandz@gmail.com>
2019-05-15 10:30:53 +0200

    changes

---
+++ project/bootcamp-tests/greet.js
@@ -0,0 +1,7 @@
+describe("the greet function"),function greet(name){
+    console.log("Hello, " + name);
+    assert.equal("Hello, Siya",greet("Siya"));
+    return "Hello, " + "Siya";
+
+}  
+
---
+++ project/bootcamp-tests/mostProfitableDepartment.js
@@ -0,0 +1,11 @@
+function mostProfitableDepartment(shop){
+    //console.log(shop)
+      var gainingDepartment=[];
+      var ourDepartment = '';
+      //console.log(gainingDepartment);
+      for(var i=0;i<shop.length;i++){
+      if(shop[i].sales)
+      gainingDepartment.push(shop[i].sales) 
+        
+      }
+      };
